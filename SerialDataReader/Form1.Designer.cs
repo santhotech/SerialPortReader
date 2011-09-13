64be6f0db196bb8d12ec93b34336b8d7fddff03a
Santhosh Kumar <santho89@gmail.com>
2011-09-13 14:30:36 +0530

    added a method to get the initial inputs and initialize serial port connection

--- SerialDataReader/Form1.Designer.cs
+++ SerialDataReader/Form1.Designer.cs
@@ -29,8 +29,12 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.portsBox = new System.Windows.Forms.ComboBox();
+            this.baudTxt = new System.Windows.Forms.TextBox();
+            this.stopTxt = new System.Windows.Forms.TextBox();
+            this.pariTxt = new System.Windows.Forms.TextBox();
+            this.dataTxt = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -38,35 +42,75 @@
             this.textBox1.Location = new System.Drawing.Point(12, 48);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(412, 246);
+            this.textBox1.Size = new System.Drawing.Size(455, 246);
             this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 22);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(129, 22);
+            this.button1.Location = new System.Drawing.Point(392, 19);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.Size = new System.Drawing.Size(75, 22);
             this.button1.TabIndex = 2;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // portsBox
+            // 
+            this.portsBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.portsBox.FormattingEnabled = true;
+            this.portsBox.Location = new System.Drawing.Point(12, 21);
+            this.portsBox.Name = "portsBox";
+            this.portsBox.Size = new System.Drawing.Size(111, 21);
+            this.portsBox.TabIndex = 3;
+            // 
+            // baudTxt
+            // 
+            this.baudTxt.Location = new System.Drawing.Point(129, 21);
+            this.baudTxt.Name = "baudTxt";
+            this.baudTxt.Size = new System.Drawing.Size(63, 20);
+            this.baudTxt.TabIndex = 4;
+            this.baudTxt.Text = "BAUD";
+            // 
+            // stopTxt
+            // 
+            this.stopTxt.Location = new System.Drawing.Point(198, 21);
+            this.stopTxt.Name = "stopTxt";
+            this.stopTxt.Size = new System.Drawing.Size(52, 20);
+            this.stopTxt.TabIndex = 5;
+            this.stopTxt.Text = "STOP";
+            // 
+            // pariTxt
+            // 
+            this.pariTxt.Location = new System.Drawing.Point(256, 21);
+            this.pariTxt.Name = "pariTxt";
+            this.pariTxt.Size = new System.Drawing.Size(52, 20);
+            this.pariTxt.TabIndex = 5;
+            this.pariTxt.Text = "PARITY";
+            // 
+            // dataTxt
+            // 
+            this.dataTxt.Location = new System.Drawing.Point(314, 21);
+            this.dataTxt.Name = "dataTxt";
+            this.dataTxt.Size = new System.Drawing.Size(72, 20);
+            this.dataTxt.TabIndex = 6;
+            this.dataTxt.Text = "DATA";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 306);
+            this.ClientSize = new System.Drawing.Size(479, 306);
+            this.Controls.Add(this.dataTxt);
+            this.Controls.Add(this.pariTxt);
+            this.Controls.Add(this.stopTxt);
+            this.Controls.Add(this.baudTxt);
+            this.Controls.Add(this.portsBox);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Serial Data Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,8 +119,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox portsBox;
+        private System.Windows.Forms.TextBox baudTxt;
+        private System.Windows.Forms.TextBox stopTxt;
+        private System.Windows.Forms.TextBox pariTxt;
+        private System.Windows.Forms.TextBox dataTxt;
     }
 }
 
--- SerialDataReader/Form1.cs
+++ SerialDataReader/Form1.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO.Ports;
 
 namespace SerialDataReader
 {
@@ -13,6 +16,45 @@ namespace SerialDataReader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             portsBox.Items.AddRange(SerialPort.GetPortNames());        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int baud =  Int32.Parse(baudTxt.Text);
+            string stop = stopTxt.Text;
+            int data = Int32.Parse(dataTxt.Text);
+            string pari = pariTxt.Text;
+            Parity pariAct = Parity.None;
+            if (pari == "Even")
+            {
+                pariAct = Parity.Even;
+            }
+            else if (pari == "Odd")
+            {
+                pariAct = Parity.Odd;
+            }
+
+            StopBits stopAct = StopBits.None;
+            if (pari == "One")
+            {
+                stopAct = StopBits.One;
+            }
+            else if (pari == "Two")
+            {
+                stopAct = StopBits.Two;
+            }
+            
+            string port = portsBox.Text;
+
+
+            SerialPort _serialPort = new SerialPort(port, baud, pariAct, data, stopAct);
+            _serialPort.Handshake = Handshake.None;
+
         }
     }
 }
